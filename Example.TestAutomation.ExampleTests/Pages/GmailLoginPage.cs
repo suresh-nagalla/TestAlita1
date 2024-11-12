15837a6c86832f7dfde940c09726d8f6e21fbeb8
suresh-nagalla <suri.nagalla@gmail.com>
2024-11-12 23:38:32 +0530

    Create Example.TestAutomation.ExampleTests/Pages/GmailLoginPage.cs

---
+++ Example.TestAutomation.ExampleTests/Pages/GmailLoginPage.cs
@@ -0,0 +1,9 @@
+using Example.TestAutomation.Helpers;
+
+namespace Example.TestAutomation.ExampleTests.Pages
+{
+    public class GmailLoginPage : IPage
+    {
+        // Define properties and methods for Gmail login page interactions here
+    }
+}
