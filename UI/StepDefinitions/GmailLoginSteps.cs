881a1f0c072d2b7289104f55eb8f95cd8b0c0906
suresh-nagalla <suri.nagalla@gmail.com>
2024-11-29 19:31:40 +0530

    Update UI/StepDefinitions/GmailLoginSteps.cs

--- UI/StepDefinitions/GmailLoginSteps.cs
+++ UI/StepDefinitions/GmailLoginSteps.cs
@@ -8,6 +8,36 @@ namespace AutomationFramework.UI.StepDefinitions
 {
     [Binding]
     public class GmailLoginSteps
+{
+    private readonly ScenarioContext _scenarioContext;
+    private readonly GmailLoginPage _gmailLoginPage;
+    private readonly ILogger _logger;
+
+    public GmailLoginSteps(ScenarioContext scenarioContext, GmailLoginPage gmailLoginPage, ILogger logger)
+    {
+        _scenarioContext = scenarioContext;
+        _gmailLoginPage = gmailLoginPage;
+        _logger = logger;
+    }
+
+    [Given("I am on the Gmail login page")]
+    public void GivenIAmOnTheGmailLoginPage()
+    {
+        _gmailLoginPage.NavigateToLoginPage();
+    }
+
+    [When("I enter a valid username in the username text box")]
+    public void WhenIEnterAValidUsernameInTheUsernameTextBox()
+    {
+        _gmailLoginPage.EnterUsername("valid.username@gmail.com");
+    }
+
+    [Then("I should see the next button enabled")]
+    public void ThenIShouldSeeTheNextButtonEnabled()
+    {
+        _gmailLoginPage.IsNextButtonEnabled().Should().BeTrue();
+    }
+}
     {
         private readonly IWebDriver _driver;
         private readonly GmailLoginBusinessLogic _gmailLoginBusinessLogic;
